25507317edb0ae7c2232eb6e01590fe7763bd92f
chaiyaphat <chaiyaphat.b5922314@email.com>
2021-03-11 14:26:56 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router()
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-    res.send("respond with a resource")
+    res.json({
+        name:art,
+        age:20
+    })
 })
 
 module.exports = router
