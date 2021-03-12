b89245c776b9716367bc8b9574613ad680b42514
chaiyaphat <chaiyaphat.b5922314@email.com>
2021-03-12 13:33:16 +0700

    add

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,7 +70,7 @@ exports.findAll_User = async (req, res) => {
     }
 }
 
-exports.find_user_byID = async (req, res) => {
+exports.find_user_byID = async (req, res) => { 
     const id = jwt_decode(req.params.id)
     try {
         const user = await Users.findById(id).select("-password")
